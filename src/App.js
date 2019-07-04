3854cda595cbf8efba5003e3c7863107fed40591
ksamy90 <ksamy90@aol.com>
2019-07-03 20:47:49 -0700

    breaking out homepage and directory

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
-import HomePage from './homepage.component';
+import React from 'react';
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
